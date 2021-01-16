6851c16f4aa91edda744409b683021679fdfcaac
lee <leezear@live.cn>
2021-01-16 15:53:18 +0800

    添加MagicSquare生成测试代码

---
+++ examples/src/main/java/org/chocosolver/examples/exp/MagicSquareExp.java
@@ -0,0 +1,200 @@
+/*
+ * This file is part of examples, http://choco-solver.org/
+ *
+ * Copyright (c) 2020, IMT Atlantique. All rights reserved.
+ *
+ * Licensed under the BSD 4-clause license.
+ *
+ * See LICENSE file in the project root for full license information.
+ */
+package org.chocosolver.examples.exp;
+
+import org.chocosolver.examples.AbstractProblem;
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solver;
+import org.chocosolver.solver.search.strategy.Search;
+import org.chocosolver.solver.variables.IntVar;
+import org.chocosolver.util.objects.Measurer;
+import org.chocosolver.util.tools.StringUtils;
+import org.kohsuke.args4j.Option;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static java.lang.System.out;
+import static java.util.Arrays.fill;
+import static org.chocosolver.solver.search.strategy.Search.inputOrderLBSearch;
+
+/**
+ * CSPLib prob019:<br/>
+ * "An order n magic square is a n by n matrix containing the numbers 1 to n^2, with each row,
+ * column and main diagonal equal the same sum.
+ * As well as finding magic squares, we are interested in the number of a given size that exist."
+ * <br/>
+ *
+ * @author Charles Prud'homme
+ * @since 31/03/11
+ */
+public class MagicSquareExp {
+
+    @Option(name = "-n", usage = "Magic square size.", required = true)
+    int n = 5;
+    float IN_SEC = 1000 * 1000 * 1000f;
+    IntVar[] vars;
+    String algo;
+    Model model;
+
+    public MagicSquareExp(Model model, int n, String algo) {
+        this.model = model;
+        this.n = n;
+        this.algo = algo;
+    }
+
+
+    public void buildModel() {
+//        model = new Model();
+        int ms = n * (n * n + 1) / 2;
+
+        IntVar[][] matrix = new IntVar[n][n];
+        IntVar[][] invMatrix = new IntVar[n][n];
+        vars = new IntVar[n * n];
+
+        int k = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++, k++) {
+                matrix[i][j] = model.intVar("square" + i + "," + j, 1, n * n, false);
+                vars[k] = matrix[i][j];
+                invMatrix[j][i] = matrix[i][j];
+            }
+        }
+
+        IntVar[] diag1 = new IntVar[n];
+        IntVar[] diag2 = new IntVar[n];
+        for (int i = 0; i < n; i++) {
+            diag1[i] = matrix[i][i];
+            diag2[i] = matrix[(n - 1) - i][i];
+        }
+
+        model.allDifferent(vars, algo).post();
+
+        int[] coeffs = new int[n];
+        fill(coeffs, 1);
+        for (int i = 0; i < n; i++) {
+            model.scalar(matrix[i], coeffs, "=", ms).post();
+            model.scalar(invMatrix[i], coeffs, "=", ms).post();
+        }
+        model.scalar(diag1, coeffs, "=", ms).post();
+        model.scalar(diag2, coeffs, "=", ms).post();
+
+        // Symetries breaking
+        model.arithm(matrix[0][n - 1], "<", matrix[n - 1][0]).post();
+        model.arithm(matrix[0][0], "<", matrix[n - 1][n - 1]).post();
+        model.arithm(matrix[0][0], "<", matrix[n - 1][0]).post();
+    }
+
+
+    public void configureSearch() {
+        model.getSolver().setSearch(inputOrderLBSearch(vars));
+    }
+
+    public static void main(String[] args) throws IOException {
+        String name = "MagicSquare";
+        // algorithms
+        String[] algorithms = new String[]{
+//                "AC_REGIN",
+                "ACFair",
+                "ACZhang18",
+                "ACZhang18M",
+//                "ACZhang20",
+                "AC20",
+                "WordRam",
+                "ACNaiveNew",
+                "BC",
+        };
+
+        //get local time
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("YYYY-MM-dd_HH_mm_ss");
+        String dateTime = LocalDateTime.now().format(dateTimeFormatter);
+
+        //output files
+        String outputFolder = "D:/exp/large";
+        File csv = new File(outputFolder + "//" + name + "_" + dateTime + ".csv");
+        BufferedWriter bw = new BufferedWriter(new FileWriter(csv, false));
+        bw.write("instance");
+        for (int i = 0; i < algorithms.length; i++) {
+            bw.write(",algorithm,node,time,matchingTime,filterTime,numDelValuesP1,numDelValuesP2,numProp,numNone,numSkip,numP1,numP2,numP1AndP2,maxArity");
+//                    bw.write(",node,time");
+        }
+        bw.newLine();
+        //for statistics
+        int runNum = 1;
+        long node = 0;
+        float time, matchingTime, filterTime, numDelValuesP1, numDelValuesP2, numProp, numNone, numSkip, numP1, numP2, numP1AndP2, maxArity;
+        float IN_SEC = 1000 * 1000 * 1000f;
+
+        for (int i = 4; i <= 70; i++) {
+            // instance name
+            bw.write(name + "-" + i);
+            for (String algorithm : algorithms) {
+                // initial
+                time = 0f;
+                matchingTime = 0f;
+                filterTime = 0f;
+                numDelValuesP1 = 0f;
+                numDelValuesP2 = 0f;
+                numProp = 0f;
+                numNone = 0f;
+                numSkip = 0f;
+                numP1 = 0f;
+                numP2 = 0f;
+                numP1AndP2 = 0f;
+                maxArity = 0f;
+                dateTime = LocalDateTime.now().format(dateTimeFormatter);
+                out.println(algorithm + " [" + dateTime + "]======>");
+
+
+                Measurer.initial();
+                Measurer.maxAllDiffArity = 0l;
+                Model model = new Model();
+                MagicSquareExp instances = new MagicSquareExp(model, 5, algorithm);
+                instances.buildModel();
+
+                Solver solver = model.getSolver();
+                solver.limitTime("1200s");
+                // heu
+                solver.setSearch(Search.defaultSearch(model));
+//                solver.setSearch(inputOrderLBSearch(instances.vars));
+
+                solver.solve();
+
+                node = solver.getNodeCount();
+                time += solver.getTimeCount() / runNum;
+                matchingTime += Measurer.matchingTime / IN_SEC / runNum;
+                filterTime += Measurer.filterTime / IN_SEC / runNum;
+                numDelValuesP1 += Measurer.numDelValuesP1 / runNum;
+                numDelValuesP2 += Measurer.numDelValuesP2 / runNum;
+                numProp += Measurer.numProp / runNum;
+                numNone += Measurer.numNone / runNum;
+                numSkip += Measurer.numSkip / runNum;
+                numP1 += Measurer.numP1 / runNum;
+                numP2 += Measurer.numP2 / runNum;
+                numP1AndP2 += Measurer.numP1AndP2 / runNum;
+                maxArity += Measurer.maxAllDiffArity / runNum;
+
+                bw.write("," + algorithm + "," + node + "," + time + "," + matchingTime + "," + filterTime + "," + numDelValuesP1 + "," + numDelValuesP2 + "," + numProp
+                        + "," + numNone + "," + numSkip + "," + numP1 + "," + numP2 + "," + numP1AndP2 + "," + maxArity);
+//                        System.out.println("," + algorithm + "," + node + "," + time + "," + matchingTime + "," + filterTime + "," + numDelValuesP1 + "," + numDelValuesP2 + "," + numProp
+//                                + "," + numNone + "," + numSkip + "," + numP1 + "," + numP2 + "," + numP1AndP2 + "," + numP1AndP2 + "," + maxArity);
+//                        bw.write("," + node + "," + time);
+                bw.flush();
+            }
+            bw.newLine();
+        }
+        bw.close();
+    }
+}
